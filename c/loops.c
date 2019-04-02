888f994604920459624d8e328f53f0c3c089b988
Alex Antsiferov <wetterkrank@gmail.com>
2019-04-03 00:17:54 +0200

    loops study in C

--- c/loops.c
+++ c/loops.c
@@ -1,22 +1,23 @@
 #include "stdio.h"
 
-int main() {
-    
-    int arr[8] = {1,1,2,3,5,8,13,21};
-    size_t arrlen = sizeof(arr) / sizeof(int);
+int main()
+{
+    int arr[8] = {1, 1, 2, 3, 5, 8, 13, 21};
 
+    size_t arrlen = sizeof(arr) / sizeof(int);
     int i = 0, sumW = 0, prodF = 1;
 
-    while(i<arrlen) {
-        //Debug: printf("Debug; i: %d, arr[i]: %d, sum: %d\n", i, arr[i], sum);
+    while (i < arrlen)
+    {
         sumW = sumW + arr[i];
         i = i + 1;
     };
-    printf("WHILE loop; Array sum: %d\n", sumW);
 
-    for (i = 0; i < arrlen; i = i + 2) {
+    for (i = 0; i < arrlen; i = i + 2)
+    {
         prodF = prodF * arr[i];
     };
-    printf("FOR loop; Array product: %d\n", prodF);
 
+    printf("WHILE loop; Array sum: %d\n", sumW);
+    printf("FOR loop; Array product: %d\n", prodF);
 };
