301f57787cf8885c2d2fb713cd942453abf568b5
Alex Antsiferov <wetterkrank@gmail.com>
2020-02-22 00:39:50 +0100

    parsing UM instructions into operator and registers

--- c/um/um.c
+++ c/um/um.c
@@ -1,41 +1,86 @@
 // Task: http://boundvariable.org/task.shtml
+// Max UM program length: UINT_MAX
 
 #include "stdio.h"
+#include "arpa/inet.h"
 
-void printUIntArr(const void *array, long len)
+int bitPrint (unsigned num)
 {
-    unsigned *arrayInt = (unsigned *)array;
-    for (long i = 0; i < len; i++)
-    {
-        printf("%d: %u \n", i, arrayInt[i]);
+  unsigned int size = sizeof(unsigned int);
+  unsigned int maxPow = 1 << (size*8-1);
+  int i=0;
+  for(; i<size*8; ++i){
+      // print last bit and shift left
+      printf("%u",num & maxPow ? 1 : 0);
+      num = num << 1;
+  }
+  printf("\n");
+}
+
+int getOperator(unsigned instruction, int* op, int* A, int* B, int* C, unsigned* V)
+// reads specified bits of the instruction
+{
+    *op = (instruction & 0xf0000000) >> 28; // bits 28-32
+
+    if (*op != 13) {
+        *C = (instruction & 7); // bits 1-3
+        *B = (instruction & 56) >> 3; // bits 4-6
+        *A = (instruction & 448) >> 6; // bits 7-9
+    }
+    else {
+        *A = (instruction & 234881024) >> 25; // bits 26-28 (mask 2^25+2^26+2^27)
+        *V = (instruction & 33554431); // bits 1-25
     };
-    printf("\n");
+
+    return 0;
 };
 
-void printChArr(const void *array, long len)
+int spinCycle(unsigned int *array, unsigned len)
 {
-    char *arrayChar = (char *)array;
-    for (long i = 0; i < len; i++)
+    // init
+    unsigned registers[8] = {0};
+    unsigned value;
+    int operator, regA, regB, regC;
+
+    for (unsigned i = 0; i < len; i++)
     {
-        printf("%d: %c \n", i, arrayChar[i]);
+        #ifdef DEBUG
+            printf("4 bytes, l'l endian: "); bitPrint(array[i]);
+        #endif
+
+        unsigned int instruction = htonl(array[i]); // 4 bytes to big endian
+
+        getOperator(instruction, &operator, &regA, &regB, &regC, &value);
+
+        #ifdef DEBUG
+            printf("4 bytes, big endian: "); bitPrint(instruction);
+            if (operator == 13)
+                printf("%d. op: %d, C: %d, Val: %d (%c)\n", i, operator, regC, value, value);
+            else
+                printf("%d. op: %d, A: %d, B: %d, C: %d\n", i, operator, regA, regB, regC);               
+        #endif
+
+        if (operator == 7 )
+            printf("%d. op: %d, A: %d, B: %d, C: %d\n", i, operator, regA, regB, regC);               
+        
     };
-}
+};
 
 int main (void) {
 
-    // file ops: no error-checking
+    // note: no error handling for file ops
     FILE* fp = fopen("um.um", "rb");
     fseek(fp, 0L, SEEK_END);
     long fsize = ftell(fp);
     rewind(fp);
-
-    printf("um.um size: %d\n",fsize);
-
-    char umArray[fsize];
-    fread(umArray, 1, fsize, fp);
+    
+    // read the file into an array
+    unsigned zeroArray[fsize];
+    fread(zeroArray, sizeof(zeroArray[0]), fsize/sizeof(zeroArray[0]), fp);
     fclose(fp);
 
-    // printChArr(umArray, fsize);
-    printUIntArr(umArray, fsize/sizeof(unsigned));
+    // loop through the instructions
+    spinCycle (zeroArray, fsize/sizeof(zeroArray[0]));
 
+    return 0;
 };
